e2e9bf14bac7676d19606e7a255408f3b7f8d66e
Eber Alejo <eber4202@hotmail.com>
2023-01-23 09:18:49 -0600

    ADD: morgan and @hapi/boom dependencies

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,18 @@
 import express from 'express'
+import morgan from 'morgan'
+import { config } from './config/config'
 
 export const appConfig = () => {
   const app = express()
 
   /** Load middlewares */
   app.use(express.json())
+  if (config.isProduction) {
+    app.use(morgan('tiny'))
+  } else {
+    app.use(morgan('dev'))
+  }
+  // app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
 
   return app
 }
